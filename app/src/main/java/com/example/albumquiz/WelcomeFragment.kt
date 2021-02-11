b0d68c6eaf7e33f593fa1c5d1c6152061d0b000c
Huncho Jack <meniman98@gmail.com>
2021-02-11 02:49:33 +0100

    Start up code, created a welcome fragment and enabled data binding

---
+++ app/src/main/java/com/example/albumquiz/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.albumquiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.albumquiz.databinding.FragmentWelcomeBinding
+
+
+class WelcomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        val binding: FragmentWelcomeBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_welcome, container, false
+        )
+        return binding.root
+
+
+    }
+}
